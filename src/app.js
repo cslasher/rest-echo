44a16d662b3263d5c29d65bab83b71f260e36a89
Edwin <cslasher@gmail.com>
2020-01-31 12:40:38 +0800

    Add querystring to POST

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.post('/echo', (req, res) => {
   console.log('Headers:', JSON.stringify(req.headers, null, 2));
   console.log('Body', JSON.stringify(req.body, null, 2));
+  console.log('Querystring:', JSON.stringify(req.query, null, 2));
   console.log('POST request complete');
   res.status(200).send('ok');
 });
