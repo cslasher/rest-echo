794f0ccb35473e9971ed5660a55b239877c82dac
Edwin <cslasher@gmail.com>
2020-01-31 12:08:59 +0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/echo', (req, res) => {
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Body', JSON.stringify(req.body, null, 2));
+  console.log('POST request complete');
+  res.status(200).send('ok');
+});
+
+app.get('/echo', (req, res) => {
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Querystring:', JSON.stringify(req.query, null, 2));
+  console.log('GET request complete');
+  res.status(200).send('ok');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port: ${PORT}`);
+});
