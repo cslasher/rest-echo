7a190799a0d97c2fc19f4a41b86bd1899103096e
Edwin <cslasher@gmail.com>
2020-01-31 13:25:52 +0800

    Test authcode

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.post('/echo', (req, res) => {
   console.log('Headers:', JSON.stringify(req.headers, null, 2));
   console.log('Body', JSON.stringify(req.body, null, 2));
   console.log('Querystring:', JSON.stringify(req.query, null, 2));
+  console.log('authcode', req.query.authcode);
   console.log('POST request complete');
   res.status(200).send('ok');
 });
