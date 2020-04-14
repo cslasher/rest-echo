04a1d149bf081737c6942ee55720c6496adf3f6b
Edwin <cslasher@gmail.com>
2020-04-14 16:56:43 +0800

    Restructure

--- src/app.js
+++ src/app.js
@@ -9,20 +9,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post('/echo', (req, res) => {
-  console.log('Headers:', JSON.stringify(req.headers, null, 2));
-  console.log('Body', JSON.stringify(req.body, null, 2));
-  console.log('Querystring:', JSON.stringify(req.query, null, 2));
-  console.log('authcode', req.query.authcode);
-  console.log('authcode2', req.query.authcode2);
-  console.log('POST request complete');
-  res.status(200).send('ok');
+  const response = {
+    header: req.headers,
+    body: req.body,
+    querystring: req.query,
+    type: 'POST',
+  };
+
+  res.status(200).json(response);
 });
 
 app.get('/echo', (req, res) => {
-  console.log('Headers:', JSON.stringify(req.headers, null, 2));
-  console.log('Querystring:', JSON.stringify(req.query, null, 2));
-  console.log('GET request complete');
-  res.status(200).send('ok');
+  const response = {
+    header: req.headers,
+    querystring: req.query,
+    type: 'GET',
+  };
+
+  res.status(200).json(response);
 });
 
 app.listen(PORT, () => {
