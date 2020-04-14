cd214f3a43b0b79d5201d209f1c89ac8904a22f7
Edwin <cslasher@gmail.com>
2020-04-14 21:47:53 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# REST Echo Server
+
+> A Rest echo service for debugging purpose
+
+## Installation
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm start
+```
+
+## Development setup
+
+Supports watch mode.
+
+```sh
+npm run watch
+```
+
+## Release History
+
+* 0.1.0 First release
+
+## Meta
+
+Edwin Liu – [@cslasher](https://github.com/cslasher/) 
+
+Distributed under the ISC license.
+
+
+## Contributing
+
+1. Fork it (<https://github.com/cslasher/rest-echo/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
--- src/app.js
+++ src/app.js
@@ -3,27 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post('/echo', (req, res) => {
+app.all('/', (req, res) => {
   const response = {
     header: req.headers,
+    method: req.method,
     body: req.body,
     querystring: req.query,
-    type: 'POST',
-  };
-
-  res.status(200).json(response);
-});
-
-app.get('/echo', (req, res) => {
-  const response = {
-    header: req.headers,
-    querystring: req.query,
-    type: 'GET',
+    protocal: req.protocol,
+    ip: req.ip,
+    cookies: req.cookies,
   };
 
   res.status(200).json(response);
